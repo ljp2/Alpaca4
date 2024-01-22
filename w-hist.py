ae72f0fcb1eacda6a4677f2d3c0a752c0ba795ef
ljp2 <ljp2@lehigh.edu>
2024-01-22 13:05:47 -0500

    working

---
+++ w-hist.py
@@ -0,0 +1,31 @@
+
+import pandas as pd
+
+from alpaca.data.historical import CryptoHistoricalDataClient
+from alpaca.data.requests import CryptoBarsRequest
+from alpaca.data.timeframe import TimeFrame
+from datetime import datetime, timedelta
+
+from keys import paper_apikey, paper_secretkey
+apikey = paper_apikey
+secretkey = paper_secretkey
+
+client = CryptoHistoricalDataClient(apikey, secretkey)
+
+end = datetime.utcnow()
+start = end - timedelta(minutes=5)
+
+request_params = CryptoBarsRequest(
+                        symbol_or_symbols=["BTC/USD"],
+                        timeframe=TimeFrame.Minute,
+                        start=start,
+                        end=end
+                 )
+
+bars = client.get_crypto_bars(request_params=request_params, feed="us")
+
+# access bars as list - important to note that you must access by symbol key
+# even for a single symbol request - models are agnostic to number of symbols
+b = bars["BTC/USD"]
+
+print(b[0].close)
---
+++ w-quote.py
@@ -0,0 +1,14 @@
+from alpaca.data.historical import CryptoHistoricalDataClient
+from alpaca.data.requests import CryptoLatestQuoteRequest
+
+from keys import paper_apikey, paper_secretkey
+apikey = paper_apikey
+secretkey = paper_secretkey
+
+client = CryptoHistoricalDataClient(apikey, secretkey)
+
+params = CryptoLatestQuoteRequest(symbol_or_symbols='BTC/USD')
+
+q = client.get_crypto_latest_quote(request_params=params, feed='us')['BTC/USD']
+
+print(f"{q.timestamp.strftime('%H:%M:%S')}\t{q.ask_price:.2f}")
