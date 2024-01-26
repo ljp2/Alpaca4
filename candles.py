92834d3744998614222019d4a1bc0cdcab38c491
ljp2 <ljp2@lehigh.edu>
2024-01-26 14:03:57 -0500

    working

--- candles.py
+++ candles.py
@@ -1,53 +1,54 @@
 import pandas as pd
 from datetime import datetime, timedelta
-
 import alpaca.data.models as alpaca_models
 
+class HAbars(pd.DataFrame):
+    @property
+    def _constructor(self):
+        return HAbars
 
-def calculate_heiken_ashi(df):
-    ha_df = pd.DataFrame(index=df.index, columns=['open', 'high', 'low', 'close'])
-    for i in range(len(df)):
-        if i == 0:
-            ha_df.iloc[i] = df.iloc[i]
-        else:
-            ha_df.at[df.index[i], 'open'] = (ha_df.at[df.index[i-1], 'open'] + ha_df.at[df.index[i-1], 'close']) / 2
-            ha_df.at[df.index[i], 'close'] = (df.at[df.index[i], 'open'] + df.at[df.index[i], 'high'] + df.at[df.index[i], 'low'] + df.at[df.index[i], 'close']) / 4
-            ha_df.at[df.index[i], 'high'] = max(df.at[df.index[i], 'high'], ha_df.at[df.index[i], 'open'], ha_df.at[df.index[i], 'close'])
-            ha_df.at[df.index[i], 'low'] = min(df.at[df.index[i], 'low'], ha_df.at[df.index[i], 'open'], ha_df.at[df.index[i], 'close'])
-    return ha_df
-
-class Bar:
-    def __init__(self, timestamp:datetime, open:float, high:float, low:float, close:float):
-        self.timestamp:datetime = timestamp
-        self.open:float = open
-        self.high:float = high
-        self.low:float = low
-        self.close:float = close
-
-    def __str__(self):
-        z =f'{self.timestamp.strftime("%H%M%S")} {self.open:.2f} {self.high:.2f} {self.low:.2f} {self.close:.2f}'
-        return z
-
-class Bars:
-    def __init__(self, df:pd.DataFrame):
+    def __init__(self, df=None, *args, **kwargs):
         if df is None:
-            self.last_timestamp = None
-            self.df = pd.DataFrame(columns=['timestamp', 'open', 'high', 'low', 'close'])
+            df = pd.DataFrame(columns=['timestamp', 'open', 'high', 'low', 'close'])
         else:
-            df['timestamp'] = pd.to_datetime(df['timestamp'])
-            self.df = df[open', 'high', 'low', 'close']].copy()
-            self.last_timestamp = df.index[-1]
-            
-    def add(self, bar:alpaca_models.bars.Bar):
-        if self.last_timestamp is None:
-            self.last_timestamp = bar.timestamp
-            self.df.loc[bar.timestamp] = [bar.open, bar.high, bar.low, bar.close]
+            df = df.copy()
+            for i in range(len(df)):
+                if i == 0:
+                    df.iloc[i] = df.iloc[i]
+                else:
+                    df.at[df.index[i], 'open'] = (df.at[df.index[i-1], 'open'] + df.at[df.index[i-1], 'close']) / 2
+                    df.at[df.index[i], 'close'] = (df.at[df.index[i], 'open'] + df.at[df.index[i], 'high'] + df.at[df.index[i], 'low'] + df.at[df.index[i], 'close']) / 4
+                    df.at[df.index[i], 'high'] = max(df.at[df.index[i], 'high'], df.at[df.index[i], 'open'], df.at[df.index[i], 'close'])
+                    df.at[df.index[i], 'low'] = min(df.at[df.index[i], 'low'], df.at[df.index[i], 'open'], df.at[df.index[i], 'close'])
+        super().__init__(df, *args, **kwargs)
+
+    def add_bar(self, new_bar:pd.Series):
+        ha_close = (new_bar['open'] + new_bar['high'] + new_bar['low'] + new_bar['close']) / 4
+        if len(self.bars) == 0:
+            ha_open = (new_bar['open'] + new_bar['close']) / 2
         else:
-            if bar.timestamp - self.last_timestamp > timedelta(minutes=1):
-                self.df.loc[bar.timestamp, 'high'] = max(self.df.loc[bar.timestamp, 'high'], bar.high)
-                self.df.loc[bar.timestamp, 'low'] = min(self.df.loc[bar.timestamp, 'low'], bar.low)
-                self.df.loc[bar.timestamp, 'close'] = bar.close
-            else:
-                self.last_timestamp = bar.timestamp
-                
-                
+            ha_open = (self.bars.iloc[-1]['open'] + self.bars.iloc[-1]['close']) / 2
+        ha_high = max(new_bar['high'], ha_open, ha_close)
+        ha_low = min(new_bar['low'], ha_open, ha_close)
+        self.bars = self.bars.append({'timestamp': new_bar['timestamp'], 'open': ha_open, 'high': ha_high, 'low': ha_low, 'close': ha_close}, ignore_index=True)
+
+class Bars(pd.DataFrame):
+    _metadata = ['timestamp', 'open', 'high', 'low', 'close']
+
+    def __init__(self, data=None, **kwargs):
+        if data is None:
+            data = pd.DataFrame(columns=self._metadata)
+        elif isinstance(data, dict):
+            data = pd.DataFrame(data, columns=self._metadata)
+        super().__init__(data, **kwargs)
+
+    @property
+    def _constructor(self):
+        return Bars
+
+    def add_row(self, row):
+        if isinstance(row, (pd.Series, dict)):
+            row = {key: row[key] for key in self._metadata if key in row}
+            self.loc[len(self)] = row
+        else:
+            raise ValueError("Row must be a pandas Series or a dictionary.")
