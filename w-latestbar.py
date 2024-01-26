de3806536ba48f2d90e5ffd8e49f1f85b5205420
ljp2 <ljp2@lehigh.edu>
2024-01-25 21:23:45 -0500

    working

--- w-latestbar.py
+++ w-latestbar.py
@@ -5,6 +5,8 @@ from time import sleep
 from alpaca.data.historical import CryptoHistoricalDataClient
 from alpaca.data.requests import CryptoLatestQuoteRequest
 
+from alpaca_api import getLatestCryptoBar
+
 from keys import paper_apikey, paper_secretkey
 apikey = paper_apikey
 secretkey = paper_secretkey
@@ -13,10 +15,18 @@ client = CryptoHistoricalDataClient(apikey, secretkey)
 
 params = CryptoLatestQuoteRequest(symbol_or_symbols='BTC/USD')
 
-q = client.get_crypto_latest_bar(request_params=params)['BTC/USD']
+# for i in range(120):
+#     try:
+#         q = client.get_crypto_latest_bar(request_params=params)['BTC/USD']
 
-z = "{}  {}\t{:0.2f}   {:0.2f}   {:0.2f}   {:0.2f}".format(
-    datetime.utcnow().strftime('%H:%M:%S'),
-    q.timestamp.strftime('%H:%M:%S'),  q.open, q.high, q.low, q.close)     
-print(z)
+#         z = "{}  {}\t{:0.2f}   {:0.2f}   {:0.2f}   {:0.2f}".format(
+#             datetime.utcnow().strftime('%H:%M:%S'),
+#             q.timestamp.strftime('%H:%M:%S'),  q.open, q.high, q.low, q.close)     
+#         print(datetime.now().strftime('%H:%M:%S'), z)
+#     except Exception as e:
+#         print(datetime.now().strftime('%H:%M:%S'), 'Exception')
+#         print(e, '\n')
+#     sleep(60)
+   
         
+print(getLatestCryptoBar('BTC/USD', 1))
--- w-stream-bars.py
+++ w-stream-bars.py
@@ -1,5 +1,9 @@
 from alpaca.data.live import CryptoDataStream
+from alpaca.data.historical import CryptoHistoricalDataClient
+
 import alpaca.data.models as models
+from pytz import timezone
+import pytz
 
 from keys import paper_apikey, paper_secretkey
 apikey = paper_apikey
@@ -8,28 +12,28 @@ secretkey = paper_secretkey
 crypto_stream = CryptoDataStream(apikey, secretkey)
 
 def write_bar_data(bar:models.Bar, filename):
-    z = f"{bar.timestamp},{bar.open},{bar.high},{bar.low},{bar.close},{bar.volume}\n"
+    z = f"{bar.timestamp.astimezone(timezone('US/Eastern'))},{bar.open},{bar.high},{bar.low},{bar.close},{bar.volume}\n"
     with open(filename, 'a') as file:
         file.write(z)
 
-# usage
-# write_bar_data(bar, 'output.txt')
-    
+def print_bar_data(bar:models.Bar):
+    z = f"{bar.timestamp.astimezone(timezone('US/Eastern'))},{bar.open},{bar.high},{bar.low},{bar.close},{bar.volume}\n"
+    print(z)
+
+def get_latest_crypto_bar(symbol: str, bar_minutes: int):
+    client = CryptoHistoricalDataClient(apikey, secretkey)
+    latest_quote = client.get_crypto_latest_quote('BTC/USD')
+    return latest_quote
     
 # async handler
 async def bar_data_handler(bar:models.Bar):
-    # quote data will arrive here
-    z = f"{bar.timestamp},{bar.open},{bar.high},{bar.low},{bar.close},{bar.volume}\n"
-    with open('test-stream.txt', 'a') as file:
-        file.write(z)
-    print('bar', z)
+    write_bar_data(bar, 'test-stream.txt')
+    print_bar_data(bar)
 
 async def updated_bar_data_handler(bar:models.Bar):
-    # quote data will arrive here
-    z = f"{bar.timestamp},{bar.open},{bar.high},{bar.low},{bar.close},{bar.volume}\n"
-    with open('test-stream.txt', 'a') as file:
-        file.write(z)
-    print('upd', z)
+    write_bar_data(bar, 'test-stream-upd.txt')
+    print_bar_data(bar)
+
 
 crypto_stream.subscribe_bars(bar_data_handler, "BTC/USD")
 crypto_stream.subscribe_updated_bars(updated_bar_data_handler, "BTC/USD")
