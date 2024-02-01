c70b2741b798efda99907412eae368926e0c7182
ljp2 <ljp2@lehigh.edu>
2024-02-01 17:47:38 -0500

    working

--- bars.py
+++ bars.py
@@ -18,7 +18,6 @@ ALPACA_SECRET_KEY = paper_secretkey
 import matplotlib.pyplot as plt
 
 
-
 def bars_process(queues):
     symbol = "BTC/USD"
     _tohlc_cols = ['timestamp', 'open', 'high', 'low', 'close']
@@ -26,10 +25,34 @@ def bars_process(queues):
     _ma_cols = ['maopen', 'mahigh', 'malow', 'maclose']
     _all_cols = _tohlc_cols + _ma_cols
     periods = {'open': 9, 'high': 5, 'low': 9, 'close': 5}
+    grouping_N = 5
+
     client = CryptoHistoricalDataClient(ALPACA_API_KEY, ALPACA_SECRET_KEY)
     request=CryptoLatestBarRequest(symbol_or_symbols='BTC/USD')
 
     bars = pd.DataFrame(columns=_all_cols)
+    bars_n = []
+    for i in range(grouping_N):
+        bars_n.append(pd.DataFrame(columns=_all_cols))
+
+    def update_barsN():
+        if len(bars) < grouping_N:
+            return
+        subdf = bars.iloc[-grouping_N:].copy()
+        index = subdf.index[0]
+        t = subdf.timestamp.iloc[0]
+        o = subdf.open.iloc[0]
+        c = subdf.close.iloc[-1]
+        h = subdf.high.max()
+        l = subdf.low.min()
+        mao =subdf.maopen.iloc[0]
+        mac = subdf.maclose.iloc[-1]
+        mah = subdf.mahigh.max()
+        mal = subdf.malow.min()
+        row = [t,o,h,l,c,mao,mah,mal,mac]
+        n = len(bars) % grouping_N     
+        tf = bars_n[n]
+        tf.loc[index] = row
 
     def add_row_to_bars(data_bar):
         row = {attr:getattr(data_bar, attr) for attr in _tohlc_cols}
@@ -49,15 +72,11 @@ def bars_process(queues):
         data = getHistoricalCryptoBars(symbol, start, end)
         for x in data:
             add_row_to_bars(x)
+            update_barsN()
 
 
     init_bars()
-    # queues['init_bars'].put(bars)
-    print(bars.tail(2))
-    
     last_timestamp = bars.iloc[-1].timestamp
-    
-    i = 1
     while True:
         try:
             print(f't{i}', end=' ', flush=True)
@@ -67,14 +86,10 @@ def bars_process(queues):
                 sleep(5)
                 continue
             else:
-                print()
-                i = 1
-                print('adding bar', bar)
                 add_row_to_bars(bar)
+                update_barsN
                 last_timestamp = bar.timestamp.timestamp()
-                print(bars.tail(2))
-                # sleep(50)
-                sleep(5)
+                sleep(55)
         except Exception as e:
             print(e)
             sleep(3)
