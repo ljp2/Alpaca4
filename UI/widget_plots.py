cbdcd7265414a9b98be1bfe04c747fcda78bd392
ljp2 <ljp2@lehigh.edu>
2024-02-02 13:49:07 -0500

    working

--- UI/widget_plots.py
+++ UI/widget_plots.py
@@ -6,8 +6,11 @@ from datetime import datetime, time
 from PyQt6 import QtCore
 from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow, QVBoxLayout, QHBoxLayout,  \
     QPushButton, QLabel
+from PyQt6.QtGui import QFont
 
 import pyqtgraph as pg
+from pyqtgraph import mkPen
+
 
 class TimeAxisItem(pg.DateAxisItem):
     def tickStrings(self, values, scale, spacing):
@@ -48,9 +51,13 @@ class Plots(QWidget):
         # self.p3.setYLink(self.p1)
 
         for p in [self.p1, self.p2, self.p3]:
-            p.setAxisItems({'bottom': TimeAxisItem()})
             p.setXRange(xlow, xhigh)
             p.setYRange(ylow, yhigh)
+            p.getAxis('left').setPen(mkPen('k', width=2))
+            p.setAxisItems({'bottom': TimeAxisItem()})
+            p.getAxis('left').setStyle(tickFont = QFont("Arial", 10), autoExpandTextSpace=True)
+            p.getAxis('bottom').setPen(mkPen('k', width=2))
+            p.getAxis('bottom').setStyle(tickFont = QFont("Arial", 10), autoExpandTextSpace=True)
 
         layout = QVBoxLayout()
         layout.addWidget(self.w)
